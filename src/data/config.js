a64f7224f622e6d160f36328afc1730e115577e3
Bladimir Pardo <bladimirpardomolina@gmail.com>
2021-12-12 15:11:46 -0400

    Base

---
+++ src/data/config.js
@@ -0,0 +1,2 @@
+const apikey = '7bd371223a1941be95f30064b51b0343';
+export default apikey;
---
+++ src/function/principal.js
@@ -0,0 +1,18 @@
+principal(n) {
+    if (n = 'general') {
+        return 'General'};
+    if (n = 'entertainment'){
+        return 'Entretenimiento'};
+    if (n = 'business'){
+        return 'Negocios'};
+    if (n = 'health') {
+        return 'Salud'};
+    if (n = 'science') {
+        return 'Ciencia'};
+    if (n = 'sports'){
+        return 'Deportes'};
+    if (n == 'technology'){
+        return 'Tecnologia'};
+}
+
+export default principal;
